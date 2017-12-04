747bb22b6eab26c64ec255b223fcf8f625537e87
Alexei Popkov <reystleen@gmail.com>
2017-12-04 15:41:07 +0100

    small bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule
