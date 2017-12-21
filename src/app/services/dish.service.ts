56b420ce19c85bde9724e8621a3e1ee3a5de1e6e
Alexei Popkov <reystleen@gmail.com>
2017-12-21 19:11:29 +0100

    HTTP Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -18,24 +18,28 @@ export class DishService {
   constructor(private http: Http,
     private processHTTPMsgService: ProcessHTTPMsgService) { }
 
-  getDishes(): Observable<Dish[]> {
-    return this.http.get(baseURL + 'dishes')
-                    .map(res => { return this.processHTTPMsgService.extractData(res); });
-  }
+    getDishes(): Observable<Dish[]> {
+      return this.http.get(baseURL + 'dishes')
+                      .map(res => { return this.processHTTPMsgService.extractData(res); })
+                      .catch(error => { return this.processHTTPMsgService.handleError(error); });
+    }
 
-  getDish(id: number): Observable<Dish> {
-    return  this.http.get(baseURL + 'dishes/'+ id)
-                    .map(res => { return this.processHTTPMsgService.extractData(res); });
-  }
+    getDish(id: number): Observable<Dish> {
+      return  this.http.get(baseURL + 'dishes/'+ id)
+                      .map(res => { return this.processHTTPMsgService.extractData(res); })
+                      .catch(error => { return this.processHTTPMsgService.handleError(error); });
+    }
 
-  getFeaturedDish(): Observable<Dish> {
-    return this.http.get(baseURL + 'dishes?featured=true')
-                    .map(res => { return this.processHTTPMsgService.extractData(res)[0]; });
-  }
+    getFeaturedDish(): Observable<Dish> {
+      return this.http.get(baseURL + 'dishes?featured=true')
+                      .map(res => { return this.processHTTPMsgService.extractData(res)[0]; })
+                      .catch(error => { return this.processHTTPMsgService.handleError(error); });
+    }
 
-  getDishIds(): Observable<number[]> {
-    return this.getDishes()
-      .map(dishes => { return dishes.map(dish => dish.id) });
-  }
+    getDishIds(): Observable<number[] | any> {
+      return this.getDishes()
+        .map(dishes => { return dishes.map(dish => dish.id) })
+        .catch(error => { return error; } );
+    }
 
 }
